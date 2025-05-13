51f02cd6e0aa0ca68b8cf22ca029fc40199a55ef
Ayush-Tiwari2004 <ayushtiwari2004m@gmail.com>
2025-05-13 07:24:43 +0530

    new bug related commit

--- Server/utils/socketio.js
+++ Server/utils/socketio.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors:{
-      origin:"http://localhost:5173",
+      origin:"https://happy-chat-iota.vercel.app/",
       methods:["POST", "GET"],
       credentials:true,
     }
